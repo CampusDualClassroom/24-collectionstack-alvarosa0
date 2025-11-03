92bab995019dc7b743260ee91945ffead369f5f6
Alvaro Sainz <alvaroalonsovp@outlook.es>
2025-11-03 18:04:54 +0100

    Exercise 24 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -4,17 +4,31 @@ import java.util.Stack;
 
 public class Exercise {
     public static Stack<String> createStack() {
+        Stack<String> stack = new Stack<>();
 
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+
+        return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
 
     }
 
     public static void main(String[] args) {
+        Stack<String> stack = createStack();;
+        System.out.println("Elemento en la cima: " + stack.peek());
+        System.out.println("Mostrar y vaciar la pila:");
+        printAndEmptyStack(stack);
+        System.out.println("¿La pila está vacía? " + stack.isEmpty());
+
 
     }
-
-
 
 }
